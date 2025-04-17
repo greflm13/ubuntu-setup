36ef91b2236dd76cb187a968c0f5a86767eb5efc
Flo Greistorfer <flo.greistorfer@sorogon.eu>
2025-04-17 13:05:36 +0200

    3rd party software added

---
+++ 3rd-party-software.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Create keyrings folder if it does not exist
+mkdir -p /usr/share/keyrings
+
+# Mattermost public key
+curl https://deb.packages.mattermost.com/pubkey.gpg | gpg --dearmor > /usr/share/keyrings/mattermost.gpg
+
+# Mattermost repo
+echo "deb https://deb.packages.mattermost.com stable main" > /etc/apt/sources.list.d/mattermost_stable.list
+
+# Anexia Fortimirror public key
+curl https://fortimirror.anexia.com/stable/DEB-GPG-KEY | gpg --dearmor > /usr/share/keyrings/fortimirror.anexia.gpg
+
+# Anexia Fortimirror repo
+echo "deb [arch=amd64] https://fortimirror.anexia.com/stable stable non-free" > /etc/apt/sources.list.d/fortimirror.anexia.list
+
+apt-get update
+
+# Citrix Workspace
+export DEBIAN_FRONTEND="noninteractive"
+citrix_dl_urls="$(curl -sL "https://www.citrix.com/downloads/workspace-app/linux/workspace-app-for-linux-latest.html" | grep -F "_amd64.deb?__gda__" | sed -En 's|^.*rel="(//.*amd64[^"]*)".*$|\1|p')"
+icaclient="https:$(echo "${citrix_dl_urls}" | grep "icaclient")"
+ctxusb="https:$(echo "${citrix_dl_urls}" | grep "ctxusb")"
+
+curl -sSL -o /tmp/icaclient_amd64.deb "${icaclient}"
+curl -sSL -o /tmp/ctxusb_amd64.deb "${ctxusb}"
+
+debconf-set-selections <<< "icaclient app_protection/install_app_protection select yes"
+
+apt-get install -f /tmp/icaclient_amd64.deb -y
+apt-get install -f /tmp/ctxusb_amd64.deb -y
+
+apt-get install -y evolution-ews mattermost-desktop forticlient linuxsecurity-installer
+
